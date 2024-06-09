463a4d80ca4d1fc9ee0a4bab77572e3cb85a816e
Aniket <aniketk9936279984@gmail.com>
2024-06-09 15:46:13 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HashRouter , Route, Routes } from "react-router-dom"
 import Navbar from "./Components/Navbar"
 import CraetePost from "./Components/CraetePost"
 import ShowData from "./Components/ShowData"
@@ -10,14 +10,16 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
-    <Navbar/>
+    <HashRouter >
+   <div className="mb-28">
+   <Navbar/>
+   </div>
     <Routes>
       <Route exact path="/create" element={<CraetePost/>} />
       <Route exact path="/" element={<ShowData/>} />
       <Route exact path="/edit/:id" element={<Edit/>} />
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </>
   )
 }
